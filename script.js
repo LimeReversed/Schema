7a22d9ba3446f863add933d2526771d4adaf2a88
Julia Call <54210280+Calljuly@users.noreply.github.com>
2019-12-02 13:47:09 +0100

    add
    funktion med att lägga till färg text och timmar

--- script.js
+++ script.js
@@ -5,9 +5,19 @@ function getInfo(e)
     if (e.target.tagName == "TD"){
         var text = prompt("Insert text");
         var hours = prompt("Insert hours");
+        var color = prompt("Insert color : Red, blue, green");
         
+        var minTable = document.getElementById('table');
+        var myRowIndex = e.target.parentElement.rowIndex + 1;
+        var myCellIndex = e.target.cellIndex;
+
+        for(var i = myRowIndex; i < Number(myRowIndex) + Number(hours-1); i++){
+            var a = document.getElementById(i.toString());
+            a.deleteCell(myCellIndex);
+        }
+
+        e.target.style.backgroundColor = color;
         e.target.rowSpan = hours;
-        e.target.addList.add("");
         e.target.textContent = text;
     }
 }

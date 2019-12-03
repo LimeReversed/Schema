4d4073fd5f295d582996fc0ecaf5ee470628f0f8
Lydia Nilsson <lydia.nilsson@gmail.com>
2019-12-03 10:39:11 +0100

    Cleaning up old code

--- script.js
+++ script.js
@@ -6,39 +6,6 @@ for(var td of allTableDataCells){
     td.addEventListener("click", showForm);
 }
 
-
-// function getInfo(e)
-// {
-//     if (e.target.tagName == "TD"){
-//         var text = document.getElementById("text").value;
-//         var hours = document.getElementById("nr").value;
-//         //var color = prompt("Insert color : Red, blue, green");
-        
-//         var minTable = document.getElementById('table');
-//         var myRowIndex = e.target.parentElement.rowIndex + 1;
-//         var myCellIndex = e.target.cellIndex;
-
-//         for(var i = myRowIndex; i < Number(myRowIndex) + Number(hours-1); i++){
-//             var a = document.getElementById(i.toString());
-//             a.deleteCell(myCellIndex);
-//         }
-
-//         e.target.style.backgroundColor = color;
-//         e.target.rowSpan = hours;
-//         e.target.textContent = text;
-//     }
-// }
-// $(document).ready(()=>{
-//     $('td').on('click', ()=>{
-//         $('#formContainer').show();
-//     });
-
-//     $('#btnSubmit').on('click', ()=>{
-//         $('#formContainer').hide();
-
-//     });
-// });
-
 function showForm () {
     $('#formContainer').show();
     currentTarget = this;
